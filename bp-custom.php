08a8a4b389772a22f88f44d5d4e3bea97006c70f
Irit <irit@Irits-MacBook-Pro.local>
2021-11-28 13:25:13 -0500

    re-arrange and rename buddypress tabs

--- bp-custom.php
+++ bp-custom.php
@@ -3,49 +3,25 @@
  * bp:Add a towns visited tab to buddy press
  */
 
-function buddypress_tab_towns() {
+function buddypress_towns_tabs() {
 	// Member visited town
 	bp_core_new_nav_item( array( 
-			'name' => 'Towns Visited', 
+			'name' => 'Town list', 
 			'slug' => 'towns-visited', 
 			'screen_function' => 'buddypress_towns_screen',
-			'default_subnav_slug' => 'towns-visited-list',
 			'position' => 1,
 			'show_for_displayed_user' => true,
 	) );
-	// Show list of towns that the member checked off as visited
-	bp_core_new_subnav_item( array( 
-		'name' => 'Towns Visited List', 
-		'slug' => 'towns-visited-list', 
-		'parent_url'        => trailingslashit( bp_displayed_user_domain() . 'towns-visited' ),
-		'parent_slug'       => 'towns-visited',
-		'screen_function' => 'buddypress_towns_screen',
-		'position' => 1,
-		'show_for_displayed_user' => true,
-	) );
-	// Map containing pins for all members visited towns
-	bp_core_new_subnav_item( array(
-		'name'                  => 'Map',
+	// Map containing dots for all members visited towns
+	bp_core_new_nav_item( array(
+		'name'                  => 'Visited Towns Map',
 		'slug'                  => 'map',
-		'parent_url'        => trailingslashit( bp_displayed_user_domain() . 'towns-visited' ),
-		'parent_slug'       => 'towns-visited',
-		'default_subnav_slug' => 'map',
-		'screen_function'       => 'map_screen',
-		'position'              => 75,
+		'screen_function'       => 'buddypress_map_screen',
+		'position'              => 2,
 		'show_for_displayed_user' => true,
 		) );
-	// Link to town list to checkk/uncheck visited towns
-	bp_core_new_subnav_item( array(
-		'name'              => 'Add/Remove Towns',
-		'slug'              => 'add-remove-towns',
-		'parent_url'        => trailingslashit( bp_displayed_user_domain() . 'towns-visited' ),
-		'parent_slug'       => 'towns-visited',
-		'screen_function'   => 'buddypress_add_towns_screen',
-		'position'          => 100,
-		'show_for_displayed_user' => true,
-	) );
 }
-add_action( 'bp_setup_nav', 'buddypress_tab_towns' );
+add_action( 'bp_setup_nav', 'buddypress_towns_tabs' );
 
 /**
  * bp:Define the towns screen
@@ -59,44 +35,25 @@ function buddypress_towns_screen() {
 /**
  * bp:Define the map screen
  */
-function map_screen() {
+function buddypress_map_screen() {
 	add_action( 'bp_template_title', 'map_tab_title' );
     add_action( 'bp_template_content', 'map_tab_content_content' );
     bp_core_load_template( 'buddypress/members/single/plugins' );
 }
 
-/**
- * bp:Define the mark towns screen
- */
-function buddypress_add_towns_screen() {
-	add_action( 'bp_template_title', 'add_towns_tab_title' );
-    add_action( 'bp_template_content', 'buddydev_edit_profile_group_data');
-	bp_core_load_template( 'buddypress/members/single/plugins' );
-}
-
-/***
- * bp: Display the link to edit visited towns XProfile field
- */
-function buddydev_edit_profile_group_data() {
-	$edit_towns_visited_url = bp_core_get_user_domain( get_current_user_id()) . '/profile/edit/group/2/';
-	echo '<a href='.$edit_towns_visited_url.'> <font color=blue> Click to add or remove towns from your visted towns list</font></a> ';
-}
-
 function towns_tab_title() {
   $num_of_towns_visited = count (xprofile_get_field_data('Towns Visited'));
-  echo 'You Visited ' . $num_of_towns_visited . ' Towns!<br>';
+  $edit_towns_visited_url = bp_core_get_user_domain( get_current_user_id()) . '/profile/edit/group/2/';
+  echo '<center>You Visited ' . $num_of_towns_visited . ' Towns! <br> <a href='.$edit_towns_visited_url.'> Add/remove towns from visted towns list</a></center>';
 
 }
 
 function map_tab_title() {
 	// shortcode for printing using print o matic plugin. More info:  https://pluginoven.com/plugins/print-o-matic/documentation/shortcode/
-	echo '<upperleft>'.do_shortcode ('[print-me target=".leaflet-pane"/]').'</upperleft><center>Towns Visited Map</center>';
+	
+	$edit_towns_visited_url = bp_core_get_user_domain( get_current_user_id()) . '/profile/edit/group/2/';
+	echo '<upperleft>'.do_shortcode ('[print-me target=".leaflet-pane"/]').'</upperleft>';
   }
-
-function add_towns_tab_title() {
-	echo 'Add/Remove Towns';
-  }
-
 /***
  * bp: Filter and display list of checked towns from the visited towns list Xprofile field
  */
@@ -121,23 +78,7 @@ function bpcodex_rename_profile_tabs() {
 	buddypress()->members->nav->edit_nav( array( 'name' => __( 'Notes', 'textdomain' ) ), BP_BUDDYBLOG_SLUG );
 	buddypress()->members->nav->edit_nav( array('name' => 'Add New Note',), 'edit', BP_BUDDYBLOG_SLUG );
 	buddypress()->members->nav->edit_nav( array('name' => 'Notes: We will be continuing to update this section to make it more user friendly.  Stay tuned!',), BUDDYBLOG_ARCHIVE_SLUG, BP_BUDDYBLOG_SLUG );
-	buddypress()->members->nav->edit_nav( array( 'name' => __( 'Towns Visited Map', 'textdomain' ) ), 'location' );
 }
 add_action( 'bp_actions', 'bpcodex_rename_profile_tabs' );
 
-/***
- * bp: reorder profile tabs
- */
-function bpcodex_change_notifications_nav_position() {
-	buddypress()->members->nav->edit_nav( array(
-        'position' => 1,
-    ), 'towns-visited' );
-	buddypress()->members->nav->edit_nav( array(
-        'position' => 10,
-    ), 'buddyblog' );
-	buddypress()->members->nav->edit_nav( array(
-        'position' => 12,
-    ), 'mediapress' );
-}
-add_action( 'bp_setup_nav', 'bpcodex_change_notifications_nav_position', 100 );
 ?>
